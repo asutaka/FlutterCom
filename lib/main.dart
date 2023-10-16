a2a3c4920a5b4833531dd724b9dc26650caafaa2
Nguyễn Văn Phú <phunv@ftech.ai>
2023-10-16 16:09:39 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,246 @@
+import 'dart:math';
+import 'dart:ui' as ui;
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const App(home: ExampleGradientBubbles()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+@immutable
+class App extends StatelessWidget {
+  const App({super.key, this.home});
 
-  // This widget is the root of your application.
+  final Widget? home;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Chat',
+      theme: ThemeData.dark(useMaterial3: true),
+      home: home,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+@immutable
+class ExampleGradientBubbles extends StatefulWidget {
+  const ExampleGradientBubbles({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ExampleGradientBubbles> createState() => _ExampleGradientBubblesState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExampleGradientBubblesState extends State<ExampleGradientBubbles> {
+  late final List<Message> data;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    data = MessageGenerator.generate(60, 1337);
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Theme(
+      data: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: const Color(0xFF4F4F4F),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Chat'),
+        ),
+        body: ListView.builder(
+          padding: const EdgeInsets.symmetric(vertical: 16.0),
+          reverse: true,
+          itemCount: data.length,
+          itemBuilder: (context, index) {
+            final message = data[index];
+            return MessageBubble(
+              message: message,
+              child: Text(message.text),
+            );
+          },
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+@immutable
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({
+    super.key,
+    required this.message,
+    required this.child,
+  });
+
+  final Message message;
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    final messageAlignment =
+        message.isMine ? Alignment.topLeft : Alignment.topRight;
+
+    return FractionallySizedBox(
+      alignment: messageAlignment,
+      widthFactor: 0.8,
+      child: Align(
+        alignment: messageAlignment,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 6.0, horizontal: 20.0),
+          child: ClipRRect(
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            child: BubbleBackground(
+              colors: [
+                if (message.isMine) ...const [
+                  Color(0xFF6C7689),
+                  Color(0xFF3A364B),
+                ] else ...const [
+                  Color(0xFF19B7FF),
+                  Color(0xFF491CCB),
+                ],
+              ],
+              child: DefaultTextStyle.merge(
+                style: const TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.white,
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: child,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class BubbleBackground extends StatelessWidget {
+  const BubbleBackground({
+    super.key,
+    required this.colors,
+    this.child,
+  });
+
+  final List<Color> colors;
+  final Widget? child;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+      painter: BubblePainter(
+        scrollable: Scrollable.of(context),
+        bubbleContext: context,
+        colors: colors,
+      ),
+      child: child,
+    );
+  }
+}
+
+class BubblePainter extends CustomPainter {
+  BubblePainter({
+    required ScrollableState scrollable,
+    required BuildContext bubbleContext,
+    required List<Color> colors,
+  })  : _scrollable = scrollable,
+        _bubbleContext = bubbleContext,
+        _colors = colors,
+        super(repaint: scrollable.position);
+
+  final ScrollableState _scrollable;
+  final BuildContext _bubbleContext;
+  final List<Color> _colors;
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final scrollableBox = _scrollable.context.findRenderObject() as RenderBox;
+    final scrollableRect = Offset.zero & scrollableBox.size;
+    final bubbleBox = _bubbleContext.findRenderObject() as RenderBox;
+
+    final origin =
+        bubbleBox.localToGlobal(Offset.zero, ancestor: scrollableBox);
+    final paint = Paint()
+      ..shader = ui.Gradient.linear(
+        scrollableRect.topCenter,
+        scrollableRect.bottomCenter,
+        _colors,
+        [0.0, 1.0],
+        TileMode.clamp,
+        Matrix4.translationValues(-origin.dx, -origin.dy, 0.0).storage,
+      );
+    canvas.drawRect(Offset.zero & size, paint);
+  }
+
+  @override
+  bool shouldRepaint(BubblePainter oldDelegate) {
+    return oldDelegate._scrollable != _scrollable ||
+        oldDelegate._bubbleContext != _bubbleContext ||
+        oldDelegate._colors != _colors;
+  }
+}
+
+enum MessageOwner { myself, other }
+
+@immutable
+class Message {
+  const Message({
+    required this.owner,
+    required this.text,
+  });
+
+  final MessageOwner owner;
+  final String text;
+
+  bool get isMine => owner == MessageOwner.myself;
+}
+
+class MessageGenerator {
+  static List<Message> generate(int count, [int? seed]) {
+    final random = Random(seed);
+    return List.unmodifiable(List<Message>.generate(count, (index) {
+      return Message(
+        owner: random.nextBool() ? MessageOwner.myself : MessageOwner.other,
+        text: _exampleData[random.nextInt(_exampleData.length)],
+      );
+    }));
+  }
+
+  static final _exampleData = [
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    'In tempus mauris at velit egestas, sed blandit felis ultrices.',
+    'Ut molestie mauris et ligula finibus iaculis.',
+    'Sed a tempor ligula.',
+    'Test',
+    'Phasellus ullamcorper, mi ut imperdiet consequat, nibh augue condimentum nunc, vitae molestie massa augue nec erat.',
+    'Donec scelerisque, erat vel placerat facilisis, eros turpis egestas nulla, a sodales elit nibh et enim.',
+    'Mauris quis dignissim neque. In a odio leo. Aliquam egestas egestas tempor. Etiam at tortor metus.',
+    'Quisque lacinia imperdiet faucibus.',
+    'Proin egestas arcu non nisl laoreet, vitae iaculis enim volutpat. In vehicula convallis magna.',
+    'Phasellus at diam a sapien laoreet gravida.',
+    'Fusce maximus fermentum sem a scelerisque.',
+    'Nam convallis sapien augue, malesuada aliquam dui bibendum nec.',
+    'Quisque dictum tincidunt ex non lobortis.',
+    'In hac habitasse platea dictumst.',
+    'Ut pharetra ligula libero, sit amet imperdiet lorem luctus sit amet.',
+    'Sed ex lorem, lacinia et varius vitae, sagittis eget libero.',
+    'Vestibulum scelerisque velit sed augue ultricies, ut vestibulum lorem luctus.',
+    'Pellentesque et risus pretium, egestas ipsum at, facilisis lectus.',
+    'Praesent id eleifend lacus.',
+    'Fusce convallis eu tortor sit amet mattis.',
+    'Vivamus lacinia magna ut urna feugiat tincidunt.',
+    'Sed in diam ut dolor imperdiet vehicula non ac turpis.',
+    'Praesent at est hendrerit, laoreet tortor sed, varius mi.',
+    'Nunc in odio leo.',
+    'Praesent placerat semper libero, ut aliquet dolor.',
+    'Vestibulum elementum leo metus, vitae auctor lorem tincidunt ut.',
+  ];
 }
