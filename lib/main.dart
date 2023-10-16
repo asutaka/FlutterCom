d64388d8bad27064b516ea6bd6fd8edd1e93e4cd
Nguyễn Văn Phú <phunv@ftech.ai>
2023-10-16 16:13:30 +0700

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,412 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MaterialApp(
+      home: ExampleDragAndDrop(),
+      debugShowCheckedModeBanner: false,
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+const List<Item> _items = [
+  Item(
+    name: 'Spinach Pizza',
+    totalPriceCents: 1299,
+    uid: '1',
+    imageProvider: NetworkImage('https://flutter'
+        '.dev/docs/cookbook/img-files/effects/split-check/Food1.jpg'),
+  ),
+  Item(
+    name: 'Veggie Delight',
+    totalPriceCents: 799,
+    uid: '2',
+    imageProvider: NetworkImage('https://flutter'
+        '.dev/docs/cookbook/img-files/effects/split-check/Food2.jpg'),
+  ),
+  Item(
+    name: 'Chicken Parmesan',
+    totalPriceCents: 1499,
+    uid: '3',
+    imageProvider: NetworkImage('https://flutter'
+        '.dev/docs/cookbook/img-files/effects/split-check/Food3.jpg'),
+  ),
+];
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+@immutable
+class ExampleDragAndDrop extends StatefulWidget {
+  const ExampleDragAndDrop({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<ExampleDragAndDrop> createState() => _ExampleDragAndDropState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _ExampleDragAndDropState extends State<ExampleDragAndDrop>
+    with TickerProviderStateMixin {
+  final List<Customer> _people = [
+    Customer(
+      name: 'Makayla',
+      imageProvider: const NetworkImage('https://flutter'
+          '.dev/docs/cookbook/img-files/effects/split-check/Avatar1.jpg'),
+    ),
+    Customer(
+      name: 'Nathan',
+      imageProvider: const NetworkImage('https://flutter'
+          '.dev/docs/cookbook/img-files/effects/split-check/Avatar2.jpg'),
+    ),
+    Customer(
+      name: 'Emilio',
+      imageProvider: const NetworkImage('https://flutter'
+          '.dev/docs/cookbook/img-files/effects/split-check/Avatar3.jpg'),
+    ),
+  ];
 
-  void _incrementCounter() {
+  final GlobalKey _draggableKey = GlobalKey();
+
+  void _itemDroppedOnCustomerCart({
+    required Item item,
+    required Customer customer,
+  }) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      customer.items.add(item);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: const Color(0xFFF7F7F7),
+      appBar: _buildAppBar(),
+      body: _buildContent(),
+    );
+  }
+
+  PreferredSizeWidget _buildAppBar() {
+    return AppBar(
+      iconTheme: const IconThemeData(color: Color(0xFFF64209)),
+      title: Text(
+        'Order Food',
+        style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+              fontSize: 36,
+              color: const Color(0xFFF64209),
+              fontWeight: FontWeight.bold,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+      ),
+      backgroundColor: const Color(0xFFF7F7F7),
+      elevation: 0,
+    );
+  }
+
+  Widget _buildContent() {
+    return Stack(
+      children: [
+        SafeArea(
+          child: Column(
+            children: [
+              Expanded(
+                child: _buildMenuList(),
+              ),
+              _buildPeopleRow(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMenuList() {
+    return ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: _items.length,
+      separatorBuilder: (context, index) {
+        return const SizedBox(
+          height: 12,
+        );
+      },
+      itemBuilder: (context, index) {
+        final item = _items[index];
+        return _buildMenuItem(
+          item: item,
+        );
+      },
+    );
+  }
+
+  Widget _buildMenuItem({
+    required Item item,
+  }) {
+    return LongPressDraggable<Item>(
+      data: item,
+      dragAnchorStrategy: pointerDragAnchorStrategy,
+      feedback: DraggingListItem(
+        dragKey: _draggableKey,
+        photoProvider: item.imageProvider,
+      ),
+      child: MenuListItem(
+        name: item.name,
+        price: item.formattedTotalItemPrice,
+        photoProvider: item.imageProvider,
+      ),
+    );
+  }
+
+  Widget _buildPeopleRow() {
+    return Container(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8,
+        vertical: 20,
+      ),
+      child: Row(
+        children: _people.map(_buildPersonWithDropZone).toList(),
+      ),
+    );
+  }
+
+  Widget _buildPersonWithDropZone(Customer customer) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 6,
+        ),
+        child: DragTarget<Item>(
+          builder: (context, candidateItems, rejectedItems) {
+            return CustomerCart(
+              hasItems: customer.items.isNotEmpty,
+              highlighted: candidateItems.isNotEmpty,
+              customer: customer,
+            );
+          },
+          onAccept: (item) {
+            _itemDroppedOnCustomerCart(
+              item: item,
+              customer: customer,
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CustomerCart extends StatelessWidget {
+  const CustomerCart({
+    super.key,
+    required this.customer,
+    this.highlighted = false,
+    this.hasItems = false,
+  });
+
+  final Customer customer;
+  final bool highlighted;
+  final bool hasItems;
+
+  @override
+  Widget build(BuildContext context) {
+    final textColor = highlighted ? Colors.white : Colors.black;
+
+    return Transform.scale(
+      scale: highlighted ? 1.075 : 1.0,
+      child: Material(
+        elevation: highlighted ? 8 : 4,
+        borderRadius: BorderRadius.circular(22),
+        color: highlighted ? const Color(0xFFF64209) : Colors.white,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 24,
+          ),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ClipOval(
+                child: SizedBox(
+                  width: 46,
+                  height: 46,
+                  child: Image(
+                    image: customer.imageProvider,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                customer.name,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: textColor,
+                      fontWeight:
+                          hasItems ? FontWeight.normal : FontWeight.bold,
+                    ),
+              ),
+              Visibility(
+                visible: hasItems,
+                maintainState: true,
+                maintainAnimation: true,
+                maintainSize: true,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 4),
+                    Text(
+                      customer.formattedTotalItemPrice,
+                      style: Theme.of(context).textTheme.bodySmall!.copyWith(
+                            color: textColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      '${customer.items.length} item${customer.items.length != 1 ? 's' : ''}',
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            color: textColor,
+                            fontSize: 12,
+                          ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MenuListItem extends StatelessWidget {
+  const MenuListItem({
+    super.key,
+    this.name = '',
+    this.price = '',
+    required this.photoProvider,
+    this.isDepressed = false,
+  });
+
+  final String name;
+  final String price;
+  final ImageProvider photoProvider;
+  final bool isDepressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 12,
+      borderRadius: BorderRadius.circular(20),
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(12),
+              child: SizedBox(
+                width: 120,
+                height: 120,
+                child: Center(
+                  child: AnimatedContainer(
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.easeInOut,
+                    height: isDepressed ? 115 : 120,
+                    width: isDepressed ? 115 : 120,
+                    child: Image(
+                      image: photoProvider,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontSize: 18,
+                        ),
+                  ),
+                  const SizedBox(height: 10),
+                  Text(
+                    price,
+                    style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                  ),
+                ],
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DraggingListItem extends StatelessWidget {
+  const DraggingListItem({
+    super.key,
+    required this.dragKey,
+    required this.photoProvider,
+  });
+
+  final GlobalKey dragKey;
+  final ImageProvider photoProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    return FractionalTranslation(
+      translation: const Offset(-0.5, -0.5),
+      child: ClipRRect(
+        key: dragKey,
+        borderRadius: BorderRadius.circular(12),
+        child: SizedBox(
+          height: 150,
+          width: 150,
+          child: Opacity(
+            opacity: 0.85,
+            child: Image(
+              image: photoProvider,
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+@immutable
+class Item {
+  const Item({
+    required this.totalPriceCents,
+    required this.name,
+    required this.uid,
+    required this.imageProvider,
+  });
+  final int totalPriceCents;
+  final String name;
+  final String uid;
+  final ImageProvider imageProvider;
+  String get formattedTotalItemPrice =>
+      '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
+}
+
+class Customer {
+  Customer({
+    required this.name,
+    required this.imageProvider,
+    List<Item>? items,
+  }) : items = items ?? [];
+
+  final String name;
+  final ImageProvider imageProvider;
+  final List<Item> items;
+
+  String get formattedTotalItemPrice {
+    final totalPriceCents =
+        items.fold<int>(0, (prev, item) => prev + item.totalPriceCents);
+    return '\$${(totalPriceCents / 100.0).toStringAsFixed(2)}';
   }
 }
